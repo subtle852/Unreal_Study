ca3cf3e13c2ca50e791c0f4186b50a650eaf41a6
subtle852 <subtle852@gmail.com>
2023-11-08 15:05:24 +0900

    feat: Object Path를 이용한 에셋 지정

--- Source/StudyProject/Private/WorldStatics/STorch.cpp
+++ Source/StudyProject/Private/WorldStatics/STorch.cpp
@@ -16,6 +16,12 @@ ASTorch::ASTorch()
     BodyStaticMeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BodyStaticMeshComponent"));
     BodyStaticMeshComponent->SetupAttachment(GetRootComponent());
     BodyStaticMeshComponent->SetRelativeLocation(FVector(0.f, 0.f, -30.f));
+    static ConstructorHelpers::FObjectFinder<UStaticMesh> BodyStaticMesh(TEXT("/Script/Engine.StaticMesh'/Game/StarterContent/Architecture/Pillar_50x500.Pillar_50x500'"));
+    // 경로 정보가 플레이 중에는 변경되지 않으므로, static 키워드 사용
+    if (true == BodyStaticMesh.Succeeded())
+    {
+        BodyStaticMeshComponent->SetStaticMesh(BodyStaticMesh.Object);
+    }
 
     PointLightComponent = CreateDefaultSubobject<UPointLightComponent>(TEXT("PointLightComponent"));
     PointLightComponent->SetupAttachment(GetRootComponent());
@@ -24,5 +30,10 @@ ASTorch::ASTorch()
     ParticleSystemComponent = CreateDefaultSubobject<UParticleSystemComponent>(TEXT("ParticleSystemComponent"));
     ParticleSystemComponent->SetupAttachment(GetRootComponent());
     ParticleSystemComponent->SetRelativeLocation(FVector(0.f, 0.f, 500.0f));
+    static ConstructorHelpers::FObjectFinder<UParticleSystem> ParticleTemplate(TEXT("/Script/Engine.ParticleSystem'/Game/StarterContent/Particles/P_Fire.P_Fire'"));
+    if (true == ParticleTemplate.Succeeded())
+    {
+        ParticleSystemComponent->SetTemplate(ParticleTemplate.Object);
+    }
 
 }
