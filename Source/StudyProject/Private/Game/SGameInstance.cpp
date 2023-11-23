7fbb0dc8f0320b7e084c09d097d04b48c872c9f1
subtle852 <subtle852@gmail.com>
2023-11-24 00:28:33 +0900

    feat: GameInstance DataTable 실습 with csv file

--- Source/StudyProject/Private/Game/SGameInstance.cpp
+++ Source/StudyProject/Private/Game/SGameInstance.cpp
@@ -9,30 +9,44 @@
 #include "JsonObjectConverter.h"// Json관련 헤더
 #include "UObject/SavePackage.h"// Json관련 헤더
 
-USGameInstance::USGameInstance()
-{
-	UE_LOG(LogTemp, Log, TEXT("USGameInstance() has been called."));
-
-	Name = TEXT("USGameInstance Class Default Object");
-	// CDO의 Name 속성에 저장됨.
-	// 중단점을 걸어보면 언리얼 에디터가 실행되기 전에 호출됨을 알 수 있음.
-}
+//USGameInstance::USGameInstance()
+//{
+//	UE_LOG(LogTemp, Log, TEXT("USGameInstance() has been called."));
+//
+//	Name = TEXT("USGameInstance Class Default Object");
+//	// CDO의 Name 속성에 저장됨.
+//	// 중단점을 걸어보면 언리얼 에디터가 실행되기 전에 호출됨을 알 수 있음.
+//}
 
 void USGameInstance::Init()
 {
 	Super::Init();
 	// virual로 만들어진 자식 클래스에서 부모 클래스를 지칭할 때, Super를 사용
 
-	UE_LOG(LogTemp, Log, TEXT("Init() has been called (UE_LOG)."));
+	//UE_LOG(LogTemp, Log, TEXT("Init() has been called (UE_LOG)."));
 
-	UKismetSystemLibrary::PrintString(GetWorld(), TEXT("Init() has been called (PrintString)."));
-	//GetWorld()는 UObject(월드 객체)를 전달하는 것 
+	//UKismetSystemLibrary::PrintString(GetWorld(), TEXT("Init() has been called (PrintString)."));
+	////GetWorld()는 UObject(월드 객체)를 전달하는 것 
 
-	// UE_LOG는 OutputLog에서 확인이 가능하다 (서버 프로그래머들이 주로 사용)
-	// PrintString은 ViewPort에서 확인이 가능하다 (클라 프로그래머들이 주로 사용)
+	//// UE_LOG는 OutputLog에서 확인이 가능하다 (서버 프로그래머들이 주로 사용)
+	//// PrintString은 ViewPort에서 확인이 가능하다 (클라 프로그래머들이 주로 사용)
 
-	// 해당 함수들은 디버깅용으로
-	// 디버깅이 끝나면 주석 처리를 해주는 것이 기본
+	//// 해당 함수들은 디버깅용으로
+	//// 디버깅이 끝나면 주석 처리를 해주는 것이 기본
+
+	if (false == ::IsValid(CharacterStatDataTable) || CharacterStatDataTable->GetRowMap().Num() <= 0)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Not enuough data in CharacterStatDataTable."));
+	}
+	else
+	{
+		for (int32 i = 1; i <= CharacterStatDataTable->GetRowMap().Num(); ++i)//GetRowMap().Num() 줄의 개수
+		{
+			check(nullptr != GetCharacterStatDataTableRow(i));
+		}
+
+		UE_LOG(LogTemp, Log, TEXT("No Error on CharacterStatDataTable."));
+	}
 
 #pragma region Property
 	////////////////////////////////////////
@@ -92,146 +106,146 @@ void USGameInstance::Init()
 #pragma endregion Property
 
 #pragma region Interface
-	USPigeon* Pigeon1 = NewObject<USPigeon>();
-	ISFlyable* Bird1 = Cast<ISFlyable>(Pigeon1);
-	// 이처럼 대부분 인터페이스 개념은 업캐스팅을 하기 위해 사용함
+	//USPigeon* Pigeon1 = NewObject<USPigeon>();
+	//ISFlyable* Bird1 = Cast<ISFlyable>(Pigeon1);
+	//// 이처럼 대부분 인터페이스 개념은 업캐스팅을 하기 위해 사용함
 
-	if(nullptr != Bird1)
-	Bird1->Fly();
+	//if(nullptr != Bird1)
+	//Bird1->Fly();
 
 #pragma endregion Interface
 
 #pragma region Serialization
-	// Src
-	FBirdData SrcRawData(TEXT("Pigeon17"), 17);
-	UE_LOG(LogTemp, Log, TEXT("[SrcRawData] Name: %s, ID: %d"), *SrcRawData.Name, SrcRawData.ID);
+	//// Src
+	//FBirdData SrcRawData(TEXT("Pigeon17"), 17);
+	//UE_LOG(LogTemp, Log, TEXT("[SrcRawData] Name: %s, ID: %d"), *SrcRawData.Name, SrcRawData.ID);
 
-	const FString SavedDir = FPaths::Combine(FPlatformMisc::ProjectDir(), TEXT("Saved"));// 저장 경로에 Saved 폴더를 추가 
-	UE_LOG(LogTemp, Log, TEXT("SavedDir: %s"), *SavedDir);
+	//const FString SavedDir = FPaths::Combine(FPlatformMisc::ProjectDir(), TEXT("Saved"));// 저장 경로에 Saved 폴더를 추가 
+	//UE_LOG(LogTemp, Log, TEXT("SavedDir: %s"), *SavedDir);
 
-	const FString RawDataFileName(TEXT("RawData.bin"));// 직렬화해서 저장할 파일 이름
-	FString AbsolutePathForRawData = FPaths::Combine(*SavedDir, *RawDataFileName);// SavedDir에 RawDataFileName을 붙여서 최종 저장 경로인 AbsolutePathForRawData 생성
-	UE_LOG(LogTemp, Log, TEXT("Relative path for saved file: %s"), *AbsolutePathForRawData);
-	FPaths::MakeStandardFilename(AbsolutePathForRawData);// 언리얼에 맞게 파일 이름 수정
-	UE_LOG(LogTemp, Log, TEXT("Absolute path for saved file: %s"), *AbsolutePathForRawData);
+	//const FString RawDataFileName(TEXT("RawData.bin"));// 직렬화해서 저장할 파일 이름
+	//FString AbsolutePathForRawData = FPaths::Combine(*SavedDir, *RawDataFileName);// SavedDir에 RawDataFileName을 붙여서 최종 저장 경로인 AbsolutePathForRawData 생성
+	//UE_LOG(LogTemp, Log, TEXT("Relative path for saved file: %s"), *AbsolutePathForRawData);
+	//FPaths::MakeStandardFilename(AbsolutePathForRawData);// 언리얼에 맞게 파일 이름 수정
+	//UE_LOG(LogTemp, Log, TEXT("Absolute path for saved file: %s"), *AbsolutePathForRawData);
 
-	FArchive* RawFileWriterAr = IFileManager::Get().CreateFileWriter(*AbsolutePathForRawData);
-	if (nullptr != RawFileWriterAr)
-	{
-		*RawFileWriterAr << SrcRawData;//RawFileWriterAr에 SrcRawData를 넘겨줌
-		RawFileWriterAr->Close();
-		delete RawFileWriterAr;
-		RawFileWriterAr = nullptr;
-	}
+	//FArchive* RawFileWriterAr = IFileManager::Get().CreateFileWriter(*AbsolutePathForRawData);
+	//if (nullptr != RawFileWriterAr)
+	//{
+	//	*RawFileWriterAr << SrcRawData;//RawFileWriterAr에 SrcRawData를 넘겨줌
+	//	RawFileWriterAr->Close();
+	//	delete RawFileWriterAr;
+	//	RawFileWriterAr = nullptr;
+	//}
 
-	// Dst
-	FBirdData DstRawData;
-	FArchive* RawFileReaderAr = IFileManager::Get().CreateFileReader(*AbsolutePathForRawData);
-	if (nullptr != RawFileReaderAr)
-	{
-		*RawFileReaderAr << DstRawData;//RawFileReaderAr에서 DstRawData를 끌어냄
-		RawFileReaderAr->Close();
-		delete RawFileReaderAr;
-		RawFileReaderAr = nullptr;
+	//// Dst
+	//FBirdData DstRawData;
+	//FArchive* RawFileReaderAr = IFileManager::Get().CreateFileReader(*AbsolutePathForRawData);
+	//if (nullptr != RawFileReaderAr)
+	//{
+	//	*RawFileReaderAr << DstRawData;//RawFileReaderAr에서 DstRawData를 끌어냄
+	//	RawFileReaderAr->Close();
+	//	delete RawFileReaderAr;
+	//	RawFileReaderAr = nullptr;
 
-		UE_LOG(LogTemp, Log, TEXT("[DstRawData] Name: %s, ID: %d"), *DstRawData.Name, DstRawData.ID);
-	}
+	//	UE_LOG(LogTemp, Log, TEXT("[DstRawData] Name: %s, ID: %d"), *DstRawData.Name, DstRawData.ID);
+	//}
 
-	SerializedPigeon = NewObject<USPigeon>();
-	SerializedPigeon->SetName(TEXT("Pigeon76"));
-	SerializedPigeon->SetID(76);
-	UE_LOG(LogTemp, Log, TEXT("[SerializedPigeon] Name: %s, ID: %d"), *SerializedPigeon->GetName(), SerializedPigeon->GetID());
+	//SerializedPigeon = NewObject<USPigeon>();
+	//SerializedPigeon->SetName(TEXT("Pigeon76"));
+	//SerializedPigeon->SetID(76);
+	//UE_LOG(LogTemp, Log, TEXT("[SerializedPigeon] Name: %s, ID: %d"), *SerializedPigeon->GetName(), SerializedPigeon->GetID());
 
-	const FString ObjectDataFileName(TEXT("ObjectData.bin"));
-	FString AbsolutePathForObjectData = FPaths::Combine(*SavedDir, *ObjectDataFileName);// 위에서 했던 것처럼 SavedDir에 파일이름 붙여서 최종 저장 경로 생성
-	FPaths::MakeStandardFilename(AbsolutePathForObjectData);// 언리얼에 맞게 파일 이름 수정
+	//const FString ObjectDataFileName(TEXT("ObjectData.bin"));
+	//FString AbsolutePathForObjectData = FPaths::Combine(*SavedDir, *ObjectDataFileName);// 위에서 했던 것처럼 SavedDir에 파일이름 붙여서 최종 저장 경로 생성
+	//FPaths::MakeStandardFilename(AbsolutePathForObjectData);// 언리얼에 맞게 파일 이름 수정
 
-	TArray<uint8> BufferArray;// 작성하기 쉽게 uint8 1바이트
-	FMemoryWriter MemoryWriterAr(BufferArray);
-	SerializedPigeon->Serialize(MemoryWriterAr);
+	//TArray<uint8> BufferArray;// 작성하기 쉽게 uint8 1바이트
+	//FMemoryWriter MemoryWriterAr(BufferArray);
+	//SerializedPigeon->Serialize(MemoryWriterAr);
 
-	TUniquePtr<FArchive> ObjectDataFileWriterAr = TUniquePtr<FArchive>(IFileManager::Get().CreateFileWriter(*AbsolutePathForObjectData));
-	if (nullptr != ObjectDataFileWriterAr)
-	{
-		*ObjectDataFileWriterAr << BufferArray;
-		ObjectDataFileWriterAr->Close();
+	//TUniquePtr<FArchive> ObjectDataFileWriterAr = TUniquePtr<FArchive>(IFileManager::Get().CreateFileWriter(*AbsolutePathForObjectData));
+	//if (nullptr != ObjectDataFileWriterAr)
+	//{
+	//	*ObjectDataFileWriterAr << BufferArray;
+	//	ObjectDataFileWriterAr->Close();
 
-		ObjectDataFileWriterAr = nullptr; //delete ObjectDataFileWriterAr; 와 같은 효과.
-	}
+	//	ObjectDataFileWriterAr = nullptr; //delete ObjectDataFileWriterAr; 와 같은 효과.
+	//}
 
-	TArray<uint8> BufferArrayFromObjectDataFile;
-	TUniquePtr<FArchive> ObjectDataFileReaderAr = TUniquePtr<FArchive>(IFileManager::Get().CreateFileReader(*AbsolutePathForObjectData));
-	if (nullptr != ObjectDataFileReaderAr)
-	{
-		*ObjectDataFileReaderAr << BufferArrayFromObjectDataFile;
-		ObjectDataFileReaderAr->Close();
+	//TArray<uint8> BufferArrayFromObjectDataFile;
+	//TUniquePtr<FArchive> ObjectDataFileReaderAr = TUniquePtr<FArchive>(IFileManager::Get().CreateFileReader(*AbsolutePathForObjectData));
+	//if (nullptr != ObjectDataFileReaderAr)
+	//{
+	//	*ObjectDataFileReaderAr << BufferArrayFromObjectDataFile;
+	//	ObjectDataFileReaderAr->Close();
 
-		ObjectDataFileReaderAr = nullptr;
-	}
+	//	ObjectDataFileReaderAr = nullptr;
+	//}
 
-	FMemoryReader MemoryReaderAr(BufferArrayFromObjectDataFile);
-	USPigeon* Pigeon77 = NewObject<USPigeon>();
-	Pigeon77->Serialize(MemoryReaderAr);
-	UE_LOG(LogTemp, Log, TEXT("[Pigeon77] Name: %s, ID: %d"), *Pigeon77->GetName(), Pigeon77->GetID());
+	//FMemoryReader MemoryReaderAr(BufferArrayFromObjectDataFile);
+	//USPigeon* Pigeon77 = NewObject<USPigeon>();
+	//Pigeon77->Serialize(MemoryReaderAr);
+	//UE_LOG(LogTemp, Log, TEXT("[Pigeon77] Name: %s, ID: %d"), *Pigeon77->GetName(), Pigeon77->GetID());
 
 #pragma endregion Serialization
 
 #pragma region Json
-	// 저장
-	const FString JsonDataFileName(TEXT("StudyJsonFile.txt"));
-	FString AbsolutePathForJsonData = FPaths::Combine(*SavedDir, *JsonDataFileName);
-	FPaths::MakeStandardFilename(AbsolutePathForJsonData);
+	//// 저장
+	//const FString JsonDataFileName(TEXT("StudyJsonFile.txt"));
+	//FString AbsolutePathForJsonData = FPaths::Combine(*SavedDir, *JsonDataFileName);
+	//FPaths::MakeStandardFilename(AbsolutePathForJsonData);
 
-	TSharedRef<FJsonObject> SrcJsonObject = MakeShared<FJsonObject>();// FJsonObject는 UObject 전용, FJsonValue는 값 전용 
-	FJsonObjectConverter::UStructToJsonObject(SerializedPigeon->GetClass(), SerializedPigeon, SrcJsonObject);
+	//TSharedRef<FJsonObject> SrcJsonObject = MakeShared<FJsonObject>();// FJsonObject는 UObject 전용, FJsonValue는 값 전용 
+	//FJsonObjectConverter::UStructToJsonObject(SerializedPigeon->GetClass(), SerializedPigeon, SrcJsonObject);
 
-	FString JsonOutString;
-	TSharedRef<TJsonWriter<TCHAR>> JsonWriterAr = TJsonWriterFactory<TCHAR>::Create(&JsonOutString);// 안정성을 위해 TSharedRef 사용
-	if (true == FJsonSerializer::Serialize(SrcJsonObject, JsonWriterAr))
-	{
-		FFileHelper::SaveStringToFile(JsonOutString, *AbsolutePathForJsonData);
-	}
+	//FString JsonOutString;
+	//TSharedRef<TJsonWriter<TCHAR>> JsonWriterAr = TJsonWriterFactory<TCHAR>::Create(&JsonOutString);// 안정성을 위해 TSharedRef 사용
+	//if (true == FJsonSerializer::Serialize(SrcJsonObject, JsonWriterAr))
+	//{
+	//	FFileHelper::SaveStringToFile(JsonOutString, *AbsolutePathForJsonData);
+	//}
 
-	// 읽기
-	FString JsonInString;
-	FFileHelper::LoadFileToString(JsonInString, *AbsolutePathForJsonData);
-	TSharedRef<TJsonReader<TCHAR>> JsonReaderAr = TJsonReaderFactory<TCHAR>::Create(JsonInString);
+	//// 읽기
+	//FString JsonInString;
+	//FFileHelper::LoadFileToString(JsonInString, *AbsolutePathForJsonData);
+	//TSharedRef<TJsonReader<TCHAR>> JsonReaderAr = TJsonReaderFactory<TCHAR>::Create(JsonInString);
 
-	TSharedPtr<FJsonObject> DstJsonObject;
-	if (true == FJsonSerializer::Deserialize(JsonReaderAr, DstJsonObject))// Deserialize 해서 DstJsonObject에 저장을 하는 것
-	{
-		USPigeon* Pigeon78 = NewObject<USPigeon>();
-		if (true == FJsonObjectConverter::JsonObjectToUStruct(DstJsonObject.ToSharedRef(), Pigeon78->GetClass(), Pigeon78))
-		{
-			UE_LOG(LogTemp, Log, TEXT("[Pigeon78] Name: %s, ID: %d"), *Pigeon78->GetName(), Pigeon78->GetID());
-		}
-	}
+	//TSharedPtr<FJsonObject> DstJsonObject;
+	//if (true == FJsonSerializer::Deserialize(JsonReaderAr, DstJsonObject))// Deserialize 해서 DstJsonObject에 저장을 하는 것
+	//{
+	//	USPigeon* Pigeon78 = NewObject<USPigeon>();
+	//	if (true == FJsonObjectConverter::JsonObjectToUStruct(DstJsonObject.ToSharedRef(), Pigeon78->GetClass(), Pigeon78))
+	//	{
+	//		UE_LOG(LogTemp, Log, TEXT("[Pigeon78] Name: %s, ID: %d"), *Pigeon78->GetName(), Pigeon78->GetID());
+	//	}
+	//}
 
 #pragma endregion Json
 
 #pragma region TMap
 
-	TMap<int32, FString> BirdMap;
-	BirdMap.Add(5, TEXT("Pigeon"));
-	BirdMap.Add(2, TEXT("Owl"));
-	BirdMap.Add(7, TEXT("Albatross"));
-	// BirdMap == [
-	//  { Key: 5, Value: "Pigeon"     },
-	//  { Key: 2, Value: "Owl"        },
-	//  { Key: 7, Value: "Albatross"  }
-	// ]
+	//TMap<int32, FString> BirdMap;
+	//BirdMap.Add(5, TEXT("Pigeon"));
+	//BirdMap.Add(2, TEXT("Owl"));
+	//BirdMap.Add(7, TEXT("Albatross"));
+	//// BirdMap == [
+	////  { Key: 5, Value: "Pigeon"     },
+	////  { Key: 2, Value: "Owl"        },
+	////  { Key: 7, Value: "Albatross"  }
+	//// ]
 
-	BirdMap.Add(2, TEXT("Penquin"));
-	// BirdMap == [
-	//  { Key: 5, Value: "Pigeon"     },
-	//  { Key: 2, Value: "Penquin"    },
-	//  { Key: 7, Value: "Albatross"  }
-	// ]
+	//BirdMap.Add(2, TEXT("Penquin"));
+	//// BirdMap == [
+	////  { Key: 5, Value: "Pigeon"     },
+	////  { Key: 2, Value: "Penquin"    },
+	////  { Key: 7, Value: "Albatross"  }
+	//// ]
 
-	FString* BirdIn7 = BirdMap.Find(7);
-	// *BirdIn7 == "Albatross"
-	FString* BirdIn8 = BirdMap.Find(8);
-	// *BirdIn8 == nullptr
+	//FString* BirdIn7 = BirdMap.Find(7);
+	//// *BirdIn7 == "Albatross"
+	//FString* BirdIn8 = BirdMap.Find(8);
+	//// *BirdIn8 == nullptr
 	
 #pragma endregion TMap
 }
@@ -240,6 +254,16 @@ void USGameInstance::Shutdown()
 {
 	Super::Shutdown();
 
-	UE_LOG(LogTemp, Log, TEXT("Shutdown() has been called."));
+	//UE_LOG(LogTemp, Log, TEXT("Shutdown() has been called."));
 
+}
+
+FSStatTableRow* USGameInstance::GetCharacterStatDataTableRow(int32 InLevel)
+{
+	if (true == ::IsValid(CharacterStatDataTable))
+	{
+		return CharacterStatDataTable->FindRow<FSStatTableRow>(*FString::FromInt(InLevel), TEXT(""));
+	}
+
+	return nullptr;
 }
--- Source/StudyProject/Public/Game/SGameInstance.h
+++ Source/StudyProject/Public/Game/SGameInstance.h
@@ -1,34 +1,51 @@
-// Game/SGameInstance.h
+// SGameInstance.h
 
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
+#include "Engine/DataTable.h"
 #include "SGameInstance.generated.h"
 
+USTRUCT(BlueprintType)
+struct FSStatTableRow : public FTableRowBase
+{
+    GENERATED_BODY()
+
+public:
+    FSStatTableRow()
+    {
+    }
+
+public:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float MaxHP;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float MaxEXP;
+
+};
+
 /**
- * 
+ *
  */
 UCLASS()
 class STUDYPROJECT_API USGameInstance : public UGameInstance
 {
-	GENERATED_BODY()
-	
-public:
-	USGameInstance();
-	//Ctrl + . 로 Create Declaration/ Definition 가능
+    GENERATED_BODY()
 
-	virtual void Init() override;
-	
-	virtual void Shutdown() override;
+public:
+    virtual void Init() override;
+
+    virtual void Shutdown() override;
+
+    const UDataTable* GetCharacterStatDataTable() { return CharacterStatDataTable; }
+
+    FSStatTableRow* USGameInstance::GetCharacterStatDataTableRow(int32 InLevel);
+    // InLevel은 Name을 의미
 
 private:
-	UPROPERTY()
-	FString Name;
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "USGameInstance", Meta = (AllowPrivateAccess))
+    class UDataTable* CharacterStatDataTable;
 
-	UPROPERTY()
-	TObjectPtr<class USPigeon> SerializedPigeon;
-	// class 전방선언
-	// 헤더 파일에서 다른 헤더 파일을 참조하면, 다른 헤더 파일이 수정되었을 때 이 헤더 파일도 함께 컴파일 되니
-	// 꼭 필요한 경우가 아니라면 헤더 파일에서 다른 헤더파일을 인클루드 하지 않고 전방선언을 활용하는 것이 좋음
 };
